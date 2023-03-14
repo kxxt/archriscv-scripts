61be64ce6155c8ec8991353ac2b35c3194eae6e6
kxxt <rsworktech@outlook.com>
2023-03-14 20:09:23 +0800

    feat: add more details in commit body

--- rmvpatch-pr.bash
+++ rmvpatch-pr.bash
@@ -31,7 +31,7 @@ fi
 git switch -c "$1" || git checkout "$1"
 rm -rf "$1"
 git add "$1"
-git commit -m "rmvpatch: $1"
+git commit -m "rmvpatch: $1\n\nThis package can be built without patch.\nAutomated by https://github.com/kxxt/archriscv-scripts"
 git push
 pull_request
 git co master
