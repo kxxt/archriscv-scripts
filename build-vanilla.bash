df2dead57f449432bbefa05c16a0deeebfacc314
kxxt <rsworktech@outlook.com>
2023-03-14 19:58:02 +0800

    Make it work

--- build-vanilla.bash
+++ build-vanilla.bash
@@ -5,10 +5,13 @@ set -xe
 if ! test -d "$1" ; then
     asp checkout "$1"
 fi
+
+cachedir=$(realpath ./cachedir)
+
 cd "$1/repos/"
 cd $(/bin/ls -D | python -c "import sys;split=[line for line in sys.stdin.read().split() if line.split('-')[0] in {'core', 'community', 'extra'}];print(split[0])")
 
 setconf PKGBUILD arch '(riscv64 x86_64)'
-extra-riscv64-build -- -d "$(realpath ./cachedir):/var/cache/pacman/pkg/"
+extra-riscv64-build -- -d "$cachedir:/var/cache/pacman/pkg/"
 
 
--- patch-rm-bot.bash
+++ patch-rm-bot.bash
@@ -3,9 +3,19 @@
 set -ex
 
 pkgs=$(rg -l "autoreconf" | xargs dirname  -- )
+ignore_list=$(<ignore-list)
 
 while IFS= read -r line; do
-	(cd ../packages && bash ./build-vanilla.bash "$line") && rm -r "$line"
-	echo "$line can be built from patch. Press enter to continue..."
-	read
+	if grep -q "$line" <<< "$ignore_list"; then
+		echo "Ignoring $line"
+		continue
+	fi
+	
+	if (cd ../packages && bash ./build-vanilla.bash "$line") && rm -r "$line"; 
+	then
+		echo "$line can be built from patch."
+	else
+		echo "$line can't be built from patch."
+	fi
+	read -r
 done < <(echo "$pkgs" | sed "/\./d")
